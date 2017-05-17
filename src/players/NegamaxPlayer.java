e333db7227ed6dc0689ad384e139759588d95c7a
Jan Kraemer <jan.kraemer.01@gmail.com>
2017-05-17 12:41:17 +0200

    add a FutureMove class and a Negamax Player

--- src/players/NegamaxPlayer.java
+++ src/players/NegamaxPlayer.java
@@ -1,4 +1,12 @@
-package gamecomponents;
+package players;
+
+import gamecomponents.Board;
+import gamecomponents.FutureMove;
+import gamecomponents.Move;
+import gamecomponents.StateEvaluator;
+
+import java.io.IOException;
+import java.util.LinkedList;
 
 /**
  * Copyright © 2017 Jan Krämer
@@ -13,29 +21,37 @@ package gamecomponents;
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-public class FutureMove {
+public class NegamaxPlayer extends Player {
 
-    private Move move;
-    private int score;
+    private int deep;
+    private Client client;
 
-    public FutureMove(int score, Move move) {
-        this.score = score;
-        this.move = move;
+    public NegamaxPlayer(int deep) {
+        this(deep, null);
     }
 
-    public int getScore() {
-        return score;
+    public NegamaxPlayer(int deep, Client client) {
+        this.client = client;
+        this.deep = deep;
     }
 
-    public Move getMove() {
-        return move;
+    @Override
+    public Move getMove(Board board) throws IOException {
+        FutureMove move = getNextMove(board.genMoves(),board,this.deep);
+        //   client.send(move.toString(), false);
+        //  return move;
     }
 
-    public void setMove(Move move) {
-        this.move = move;
+    private FutureMove getNextMove(LinkedList<Move> moves, Board board, int deep) {
+        if(deep == 0)
+            return new FutureMove(StateEvaluator.validateState(board),null);
+
+        return null;
     }
 
-    public void setScore(int score) {
-        this.score = score;
+
+    @Override
+    public void print(Board board, Move move) {
+        System.out.println(move + " Negamx\n" + board);
     }
 }
