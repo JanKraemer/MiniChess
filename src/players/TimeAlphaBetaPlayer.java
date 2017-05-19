011125f25988172ba0279b4eca6ecbdc254e4a40
Jan Kraemer <jan.kraemer.01@gmail.com>
2017-05-19 14:02:04 +0200

    add a TimeAlphaBetaPlayer

--- src/players/TimeAlphaBetaPlayer.java
+++ src/players/TimeAlphaBetaPlayer.java
@@ -3,47 +3,91 @@ package players;
 import gamecomponents.Board;
 import gamecomponents.FutureMove;
 import gamecomponents.Move;
+import gamecomponents.StateEvaluator;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.TimeoutException;
 
-public class TimeAlphaBetaPlayer extends Player{
+public class TimeAlphaBetaPlayer extends Player {
 
     private int deep;
     private long time;
+    private int counter;
 
-    public TimeAlphaBetaPlayer(int deep){
+    public TimeAlphaBetaPlayer(int deep) {
         this.deep = deep;
     }
 
     @Override
     public Move getMove(Board board) throws IOException {
-        return null;
+        time = System.currentTimeMillis();
+        counter = 0;
+        FutureMove move = getNextMove(new Board(board), 8500, -8500);
+        if (client != null && move != null)
+            client.send(move.getMove().toString(), false);
+        return move.getMove();
     }
 
-  /*  private FutureMove getNextMove(Board board, int beta, int alpha) {
+    private FutureMove getNextMove(Board board, int beta, int alpha) {
         int deep = this.deep;
-        FutureMove move = alphabetanegamax(board,2,beta,alpha);
-        try{
-            while(true){
-                checkTime();
-                FutureMove next = alphabetanegamax(board,deep,beta,alpha);
-                if( next.getScore() > 8500){
+        FutureMove move = null;
+        try {
+        move = alphabetanegamax(board, 4, beta, alpha);
+
+            while (true) {
+                FutureMove next = alphabetanegamax(board, deep, beta, alpha);
+                if (next.getScore() > 8500) {
                     return next;
                 }
-                if( next.getScore() < -8500){
+                if (next.getScore() < -8500) {
                     return move;
                 }
                 move = next;
                 deep++;
             }
-        }catch (TimeoutException e){
+        } catch (TimeoutException e) {
+            System.out.println(deep);
             return move;
-        }*/
-//}
+        }
+    }
+
+    private FutureMove alphabetanegamax(Board board, int deep, int beta, int alpha) throws TimeoutException {
+        counter++;
+        if (deep == 0)
+            return new FutureMove(StateEvaluator.validateState(board), null);
+        FutureMove move = new FutureMove(Integer.MIN_VALUE, null);
+        ArrayList<Move> moves = board.genMoves();
+        for (Move actualMove : moves) {
+            if(counter == 10000){
+                counter = 0;
+                checkTime();
+            }
+            int score;
+            board.move(actualMove);
+            int value_ = (int) ((StateEvaluator.validateState(board) * (-1)) * 0.9);
+            if (value_ > 8500 || value_ < -8500) {
+                score = value_;
+            } else {
+                FutureMove next = alphabetanegamax(board, deep - 1, (-1) * alpha, (-1) * beta);
+                score = (-1) * next.getScore();
+            }
+            board.rerollBoard();
+            if (score > beta)
+                return new FutureMove(value_, actualMove);
+            if (score > alpha)
+                alpha = score;
+            if (score > move.getScore()) {
+                move.setScore(score);
+                move.setMove(actualMove);
+            }
+        }
+        return move;
+    }
 
     private void checkTime() throws TimeoutException {
-        if (System.currentTimeMillis() > (time + 7E6)) {
+        long times = System.currentTimeMillis();
+        if (times >= (time + 7000)) {
             throw new TimeoutException();
         }
     }
