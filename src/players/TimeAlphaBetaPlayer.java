6e39051f245f3be641e7c95e417c1b06b0bba7cd
Jan Kraemer <jan.kraemer.01@gmail.com>
2017-05-19 13:19:42 +0200

    add TimeAlphaBot

--- src/players/TimeAlphaBetaPlayer.java
+++ src/players/TimeAlphaBetaPlayer.java
@@ -1,13 +1,16 @@
 package players;
 
 import gamecomponents.Board;
+import gamecomponents.FutureMove;
 import gamecomponents.Move;
 
 import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 public class TimeAlphaBetaPlayer extends Player{
 
     private int deep;
+    private long time;
 
     public TimeAlphaBetaPlayer(int deep){
         this.deep = deep;
@@ -16,6 +19,33 @@ public class TimeAlphaBetaPlayer extends Player{
     @Override
     public Move getMove(Board board) throws IOException {
         return null;
+    }
+
+  /*  private FutureMove getNextMove(Board board, int beta, int alpha) {
+        int deep = this.deep;
+        FutureMove move = alphabetanegamax(board,2,beta,alpha);
+        try{
+            while(true){
+                checkTime();
+                FutureMove next = alphabetanegamax(board,deep,beta,alpha);
+                if( next.getScore() > 8500){
+                    return next;
+                }
+                if( next.getScore() < -8500){
+                    return move;
+                }
+                move = next;
+                deep++;
+            }
+        }catch (TimeoutException e){
+            return move;
+        }*/
+//}
+
+    private void checkTime() throws TimeoutException {
+        if (System.currentTimeMillis() > (time + 7E6)) {
+            throw new TimeoutException();
+        }
     }
 
     @Override
