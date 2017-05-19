ad0daa6789754984d89f2fd37a6916a26dfdcb77
Jan Kraemer <jan.kraemer.01@gmail.com>
2017-05-19 15:45:03 +0200

    add docu

--- test/ComponentsTests/AlgorithmTest.java
+++ test/ComponentsTests/AlgorithmTest.java
@@ -8,9 +8,15 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/**
+ * test the alogirhm class
+ */
 public class AlgorithmTest {
 
 
+    /**
+     * check if the genMoves work fine for the first move
+     */
     @Test
     public void checkCorrectAnswerfromAlgorithm(){
         Board board = new Board();
@@ -18,6 +24,9 @@ public class AlgorithmTest {
         Assert.assertEquals(7,moves.size());
     }
 
+    /**
+     * check if the number of moves is correct
+     */
     @Test
     public void checkAnswerFromAlgorithm(){
         Board board = new Board(BoardTest.generateTestValue());
