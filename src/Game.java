b2b04fd74eb46bd65d47f92de1c4d97666432fba
Jan Kraemer <jan.kraemer.01@gmail.com>
2017-05-19 15:41:31 +0200

    adding documentation for game

--- src/Game.java
+++ src/Game.java
@@ -19,22 +19,34 @@ import java.util.HashMap;
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-public class Game {
 
+/**
+ * Game class with FluentInterface
+ */
+public class Game {
 
     private Board board;
     private HashMap<Character, Player> playerHashMap;
-
 
     protected Game(Board board, HashMap<Character, Player> playerHashMap) {
         this.board = board;
         this.playerHashMap = playerHashMap;
     }
 
+    /**
+     * provides a static method for getting a GameBuilder
+     *
+     * @return given back a new GameBuilder
+     */
     public static GameBuilder getInstance() {
         return new GameBuilder();
     }
 
+    /**
+     * start a normal game
+     *
+     * @throws IOException if the client has some problems
+     */
     public void start() throws IOException {
         char value = '?';
         while (value == '?') {
@@ -48,6 +60,11 @@ public class Game {
         System.out.println(value+ " WINS");
     }
 
+    /**
+     * provides a network game agains a clientPlayer
+     *
+     * @throws IOException if the client has some problems
+     */
     public void startNetworkGame()throws IOException {
         while (true) {
             Player actual = playerHashMap.get(board.getOnMove());
@@ -59,6 +76,13 @@ public class Game {
         }
     }
 
+    /**
+     * Provides several games for the number of rounds
+     *
+     * @param rounds given rounds to play
+     * @return a array with game ending values
+     * @throws IOException if the client is broken
+     */
     public int[] startForRounds(int rounds)throws IOException {
         int[] score = new int[3];
         int index = 0;
@@ -69,6 +93,14 @@ public class Game {
         return score;
     }
 
+    /**
+     * playing a game up to the point that a game is over
+     *
+     * @param board actual board
+     * @param count game results
+     * @return game results
+     * @throws IOException if the client has some problems
+     */
     private int[] playGame(Board board, int[] count) throws IOException {
         char value = '?';
         while (value == '?') {
@@ -89,6 +121,9 @@ public class Game {
         return count;
     }
 
+    /**
+     * static class for FluentInterface
+     */
     static class GameBuilder {
 
         private Client client;
@@ -108,6 +143,12 @@ public class Game {
             board = new Board();
         }
 
+        /**
+         * setting up the client for the server
+         *
+         * @param args given args from the cmmand line
+         * @return the class
+         */
         public GameBuilder withClient(String[] args) {
             method = args[0];
             if (args.length == 2) {
@@ -124,6 +165,12 @@ public class Game {
             return this;
         }
 
+        /**
+         * setting up a game without a client,
+         * its only for games agains myself or a random player
+         *
+         * @return myself
+         */
         public GameBuilder withoutClient() {
             client = null;
             return this;
@@ -144,6 +191,13 @@ public class Game {
             }
         }
 
+        /**
+         * set a player to the board and the other player will be
+         * a client player
+         *
+         * @param myPlayer the player the user choose
+         * @return myself
+         */
         public GameBuilder setPlayer(Player myPlayer) {
             myPlayer.setClient(client);
             ClientPlayer clientPlayer = new ClientPlayer();
@@ -156,6 +210,13 @@ public class Game {
             return this;
         }
 
+        /**
+         * Setting up both players to the game
+         *
+         * @param player1 white player
+         * @param player2 black player
+         * @return
+         */
         public GameBuilder setPlayers(Player player1, Player player2) {
             players.put('W', player1);
             players.put('B', player2);
@@ -163,6 +224,11 @@ public class Game {
             return this;
         }
 
+        /**
+         * finish the building
+         *
+         * @return a Game
+         */
         public Game finish() {
             return new Game(board, players);
         }
