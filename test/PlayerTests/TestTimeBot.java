3a498178666720fc41eb93b896d2ccf23760a3a2
Jan Kraemer <jan.kraemer.01@gmail.com>
2017-05-19 14:02:31 +0200

    check if the time is getting right

--- test/PlayerTests/TestTimeBot.java
+++ test/PlayerTests/TestTimeBot.java
@@ -1,4 +1,33 @@
 package PlayerTests;
 
+import gamecomponents.Board;
+import org.junit.Test;
+import players.AlphaBetaPlayer;
+import players.Player;
+import players.TimeAlphaBetaPlayer;
+
+import java.io.IOException;
+
 public class TestTimeBot {
+
+    @Test
+    public void checkTime() throws IOException {
+        double[] times = new double[3];
+        for(int i = 0;i< 5;i++){
+            Player player = new TimeAlphaBetaPlayer(7);
+            long milis = System.nanoTime();
+            Board board = new Board();
+            times[0]+=System.nanoTime()-milis;
+            milis = System.nanoTime();
+            board.move(player.getMove(board));
+            times[1]+=System.nanoTime()-milis;
+            milis = System.nanoTime();
+            board.rerollBoard();
+            times[2]+=System.nanoTime()-milis;
+        }
+
+        System.out.println((times[0]/10)+ "ns Konstruktor");
+        System.out.println((times[1]/10)%(1E9)+ "s Make Move");
+        System.out.println((times[2]/10)+ "ns Reroll Move");
+    }
 }
